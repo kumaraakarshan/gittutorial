ffafda12edd0f69f2cc9b516db40e23f065be8cd
kumaraakarshan <aakarshan0005@gmail.com>
2023-07-28 14:44:05 +0530

    creating nodes and modifying nodes

--- dom.js
+++ dom.js
@@ -73,18 +73,106 @@
 
 //query selector all
 
-var titles=document.querySelectorAll('.title');
-console.log(titles);
-titles[0].textContent="Hello";
-var seconditem=document.querySelector('li:nth-child(2)');
-seconditem.style.color='green';
-var odd =document.querySelectorAll('li:nth-child(odd)');
-var even =document.querySelectorAll('li:nth-child(even)');
+// var titles=document.querySelectorAll('.title');
+// console.log(titles);
+// titles[0].textContent="Hello";
+// var seconditem=document.querySelector('li:nth-child(2)');
+// seconditem.style.color='green';
+// var odd =document.querySelectorAll('li:nth-child(odd)');
+// var even =document.querySelectorAll('li:nth-child(even)');
 
 
-for(var i=0;i<odd.length;i++)
-{
-odd[i].style.backgroundColor='green';
-even[i].style.backgroundColor='grey';
-}
+// for(var i=0;i<odd.length;i++)
+// {
+// odd[i].style.backgroundColor='green';
+// even[i].style.backgroundColor='grey';
+// }
+
+//traversing the dom
+
+var itemlist= document.querySelector('#items'); 
+// console.log(itemlist.parentNode);
+// itemlist.parentNode.style.backgroundColor = 'grey';
+// console.log(itemlist.parentNode.parentNode.parentNode);
+
+
+console.log(itemlist.parentElement);
+itemlist.parentElement.style.backgroundColor = 'grey';
+console.log(itemlist.parentElement.parentElement.parentElement);
+
+//child nodes//
+//console.log(itemlist.childNodes);
+// console.log(itemlist.children);
+// console.log(itemlist.children[1].style.backgroundColor ='yellow');
+
+// //firstChild
+// console.log(itemlist.firstChild);
+
+// //first element child
+// console.log(itemlist.firstElementChild);
+
+// itemlist.firstElementChild.textContent = 'Hello';
+
+
+// //last child
+// console.log(itemlist.lastChild);
+
+//last element child
+console.log(itemlist.lastElementChild);
+itemlist.lastElementChild.textContent = 'Hello last';
+
+
+//next element siblings
+console.log(itemlist.nextSibling);
+
+//next element sibling
+console.log(itemlist.nextElementSibling);
+
+//previousSibling
+console.log(itemlist.previousSibling);
+
+//previousElementSibling
+console.log(itemlist.previousElementSibling);
+itemlist.previousElementSibling.style.color ='green';
+
+//create a div
+var newDiv = document.createElement('div');
+
+//add class name
+newDiv.className= 'hello';
+//add id
+newDiv.id='hello id';
+
+//set attribute
+newDiv.setAttribute('title','hello div');
+
+//create text node
+var newDivText = document.createTextNode('Hello Word');
+
+
+
+//add text to div
+newDiv.appendChild(newDivText);
+
+
+var container=document.querySelector('header .container');
+var h1 = document.querySelector('header h1');
+
+console.log(newDiv);
+
+newDiv.style.fontSize ='30px';
+container.insertBefore(newDiv, h1);
+
+
+
+var newDiv2 = document.createElement('div');
+newDiv2.className= 'hello2';
+newDiv2.setAttribute('title','hello div2');
+
+//adding helloword before item 1
+var newDivText2 = document.createTextNode('Hello Word');
+
+newDiv2.appendChild(newDivText2);
+
+itemlist.parentNode.insertBefore(newDiv2,itemlist)
 
